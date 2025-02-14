dd2dcaa9b8c980fc65a5aced8f7e5c2af29a8a47
Martu <martavgarcia15@hotmail.com>
2025-02-14 13:16:52 +0100

    Author_2_3

---
+++ src/es/uah/matcomp/mp/e1/composagre/b/Author_2_3.java
@@ -0,0 +1,23 @@
+package es.uah.matcomp.mp.e1.composagre.b;
+
+public class Author_2_3 {
+    private String name;
+    private String email;
+
+    public Author_2_3(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+    public String getName() {
+        return name;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String toString() {
+        return "Author[" + "name=" + name + ", email=" + email + ']';
+    }
+}
