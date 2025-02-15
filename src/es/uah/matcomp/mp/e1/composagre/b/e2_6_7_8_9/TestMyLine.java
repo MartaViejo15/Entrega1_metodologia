bafa5db3ec0bd2ab37968b5cdbc6900f5c981bef
Martu <martavgarcia15@hotmail.com>
2025-02-15 21:30:38 +0100

    TestMyLine

---
+++ src/es/uah/matcomp/mp/e1/composagre/b/e2_6_7_8_9/TestMyLine.java
@@ -0,0 +1,13 @@
+package es.uah.matcomp.mp.e1.composagre.b.e2_6_7_8_9;
+
+public class TestMyLine {
+    public static void main(String[] args) {
+        // Test for defining the data
+        MyLine begin = new MyLine(4, 8, 5, 6);
+        MyLine end = new MyLine(8, 10, 7, 9);
+        System.out.println(begin);
+        System.out.println(end);
+        // Test for getters and setters
+       // begin.set
+    }
+}
