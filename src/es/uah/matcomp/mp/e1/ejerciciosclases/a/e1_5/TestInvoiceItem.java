db2314ff89248bd9e17e0fac8bc35e8a0d6cf953
Martu <martavgarcia15@hotmail.com>
2025-02-15 21:29:30 +0100

    TestInvoiceItem

--- src/es/uah/matcomp/mp/e1/ejerciciosclases/a/e1_5/TestInvoiceItem.java
+++ src/es/uah/matcomp/mp/e1/ejerciciosclases/a/e1_5/TestInvoiceItem.java
@@ -1,6 +1,6 @@
-package es.uah.matcomp.mp.e1.ejerciciosclases.a;
+package es.uah.matcomp.mp.e1.ejerciciosclases.a.e1_5;
 
-class testMaine {
+class TestInvoiceItem {
     public static void main(String[] args) {
         // Test constructor and toString()
         InvoiceItem inv1 = new InvoiceItem("A101", "Pen Red", 888, 0.08);
