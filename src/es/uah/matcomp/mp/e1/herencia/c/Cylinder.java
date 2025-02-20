da97a4e6dc3220d110ca168579b608750be5bd36
Martu <martavgarcia15@hotmail.com>
2025-02-21 00:58:29 +0100

    Cylinder

--- src/es/uah/matcomp/mp/e1/herencia/c/Cylinder.java
+++ src/es/uah/matcomp/mp/e1/herencia/c/Cylinder.java
@@ -1,16 +1,33 @@
 package es.uah.matcomp.mp.e1.herencia.c;
 
-public class Cylinder {
-    private double height;
+public class Cylinder extends Circle { // Save as "Cylinder.java"
+    private double height; // private variable
 
-    public Cylinder(){
-        this.height = 0;
+    // Constructor with default color, radius and height
+    public Cylinder() {
+        super(); // call superclass no-arg constructor Circle()
+        height = 1.0;
     }
-    public Cylinder(double radius){
-        this.height = radius;
-    }
-    public Cylinder(double radius, double height){
+    // Constructor with default radius, color but given height
+    public Cylinder(double height) {
+        super(); // call superclass no-arg constructor Circle()
         this.height = height;
-        this.radius = radius
+    }
+    // Constructor with default color, but given radius, height
+    public Cylinder(double radius, double height) {
+        super(radius); // call superclass constructor Circle(r)
+        this.height = height;
+    }
+
+    // A public method for retrieving the height
+    public double getHeight() {
+        return height;
+    }
+
+    // A public method for computing the volume of cylinder
+    // use superclass method getArea() to get the base area
+    public double getVolume() {
+        return getArea()*height;
     }
 }
+
