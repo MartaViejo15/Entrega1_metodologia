dd401c2d5e0d818155d3cc6e30028a36710f8d8c
Martu <martavgarcia15@hotmail.com>
2025-03-07 15:14:44 +0100

    6.1

---
+++ src/es/uah/matcomp/mp/e1/PolimorfAbstrInterf/d/e6_1/Circle.java
@@ -0,0 +1,34 @@
+package es.uah.matcomp.mp.e1.PolimorfAbstrInterf.d.e6_1;
+
+import es.uah.matcomp.mp.e1.herencia.c.e3_5.Shape;
+
+public class Circle extends Shape {
+    protected double radius = 1.0;
+
+    public Circle() {
+        super();
+    }
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+    public Circle(double radius, String color, boolean filled) {
+        super(color, filled);
+        this.radius = radius;
+    }
+    public double getRadius() {
+        return radius;
+    }
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+    public double getArea() {
+        return Math.PI * radius * radius;
+    }
+    public double getPerimeter() {
+        return 2 * Math.PI * radius;
+    }
+    @Override
+    public String toString() {
+        return "Circle[" + super.toString() + ", radius=" + radius + "]";
+    }
+}
---
+++ src/es/uah/matcomp/mp/e1/PolimorfAbstrInterf/d/e6_1/Square.java
@@ -0,0 +1,33 @@
+package es.uah.matcomp.mp.e1.PolimorfAbstrInterf.d.e6_1;
+
+import es.uah.matcomp.mp.e1.herencia.c.e3_5.Rectangle;
+
+public class Square extends Rectangle {
+
+    public Square() {
+    }
+    public Square(double side) {
+        super(side, side);
+    }
+    public Square(double side, String color, boolean filled) {
+        super(side, side, color, filled);
+    }
+    public double getSide(){
+        return getWidth();
+    }
+    public void setSide(double side){
+        this.setWidth(side);
+    }
+    @Override
+    public void setWidth(double side){
+        super.setWidth(side);
+    }
+    @Override
+    public void setLength(double side){
+        super.setLength(side);
+    }
+    @Override
+    public String toString(){
+        return "Square[" + super.toString() + "]";
+    }
+}
