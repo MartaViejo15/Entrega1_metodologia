7184dd3617cb353b6ff5ae8e9376f5ce546bda13
Martu <martavgarcia15@hotmail.com>
2025-03-09 12:55:36 +0100

    6.7

---
+++ src/es/uah/matcomp/mp/e1/PolimorfAbstrInterf/d/e6_7/Animal.java
@@ -0,0 +1,5 @@
+package es.uah.matcomp.mp.e1.PolimorfAbstrInterf.d.e6_7;
+
+abstract public class Animal {
+    abstract public void greeting();
+}
---
+++ src/es/uah/matcomp/mp/e1/PolimorfAbstrInterf/d/e6_7/Cat.java
@@ -0,0 +1,8 @@
+package es.uah.matcomp.mp.e1.PolimorfAbstrInterf.d.e6_7;
+
+public class Cat extends Animal {
+    @Override
+    public void greeting() {
+        System.out.println("Meow!");
+    }
+}
